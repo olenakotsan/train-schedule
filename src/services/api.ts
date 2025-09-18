8f52515d9b78e652c8312e613b8ccf1edcafdf13
olenakotsan <olenka.kotsan@gmail.com>
2025-09-18 21:50:38 +0300

    add env variable for app base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://train-schedule.railway.internal"
+    ? process.env.REACT_APP_API_BASE_URL
     : "http://localhost:3001";
 
 export const api = axios.create({
