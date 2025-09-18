f8173c7c6f528d02a46f0cddf1e3329d43e5b0cb
olenakotsan <olenka.kotsan@gmail.com>
2025-09-18 21:40:10 +0300

    fix app link

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,5 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
-
-  console.log(`🚀 Backend running on http://localhost:${port}`);
 }
 bootstrap();
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "train-schedule.railway.internal"
+    ? "https://train-schedule.railway.internal"
     : "http://localhost:3001";
 
 export const api = axios.create({
